2d2e4e757ea63e827cf6876109bfa2e1ed8d0de7
Monicastf <monicas.f555@gmail.com>
2024-11-27 14:04:09 -0700

    Update prediction.js

--- prediction.js
+++ prediction.js
@@ -8,5 +8,17 @@ response = Math.floor(Math.random() * 11); //0-10
 console.log(response);
 //response = Math.floor(Math.random() *9) +1 -> to generate 1-10
 
+if(response==0){
+    output.innerHTML = "Yes, indeed."
+}
+
+if(response==1){
+    output.innerHTML = "Ask again later."
+}
+
+if(response==2){
+    output.innerHTML = "Very doubtful."
+}
+
 }
 )
